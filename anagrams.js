82abf1ace7f53f8dd8ff181659ba83d31a638b6e
Johnathan Reese <JohnathanReese8@gmail.com>
2023-03-03 17:58:42 -0500

    Form. Not working yet.

--- anagrams.js
+++ anagrams.js
@@ -1,26 +1,48 @@
 // take a string and return anagrams
 
-var anaFormEl = document.querySelector("#anagram-form");
+var anagramFormEl = document.querySelector("#anagram-form");
 
-var ananputEl = document.querySelector("anagram");
+var anagramInputEl = document.querySelector("#anagram");
+
+var anagramSearch = document.querySelector("#anagram-search");
 
 let anagrams = [];
 
-let genAnagrams = (word, anagram = '') => {
-    word = word.toUpperCase();
+// Input word for anagram
+
+var searchButton = function(event) {
+    event.preventDefault();
+  
+    // get value from input element
+    var anagramName = anagramInputEl.value.trim();
+  
+    if (anagramName) {
+      getAnagrams();
+  
+      anagramInputEl.value = "";
+
+    } else {
+      alert("Please enter a valid word.");
+    }
+};
+
+let getAnagrams = (word, anagramName) => {
+    word = word.toUpperCase(); //makes lowercase letters = uppercase so there's no repeat amongst letters, giving an undefined error
     if (!word) {
-        anagrams.push(anagram);
+        anagrams.push(anagramName);
         return;
     }
 
     for (let i = 0; i < word.length; i++) {
         // concat current letter to anagram
-        anagram += word[i];
-        genAnagrams(word.slice(0, i) + word.slice(i + 1), anagram, anagrams); //wont include zero
-        anagram = anagram.slice(0, anagram.length -1);
+        anagramName += word[i];
+        getAnagrams(word.slice(0, i) + word.slice(i + 1), anagramName, anagrams); //wont include zero
+        anagramName = anagramName.slice(0, anagramName.length -1);
     }
 
-    return [... new set(anagrams)];
+    anagramName.push(anagramResult);
+    var anagramResult = [anagrams]
 };
 
-console.log(genAnagrams('AA'));
+
+anagramFormEl.addEventListener("submit", searchButton);
